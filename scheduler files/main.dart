5d32c0bb8fc39b582dbdc36d68fba8cf6c223ce3
Jaelen Wright <jaelen.wright@ontariotechu.net>
2023-11-10 18:13:50 -0500

    Update main.dart
    update title and add logo (placeholder) to appbar

--- scheduler files/main.dart
+++ scheduler files/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Scheduler'),
     );
   }
 }
@@ -85,6 +85,9 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        leading: const Icon(
+            Icons.school, // temporary logo
+            size: 30),
         title: Text(widget.title),
       ),
       body: Column(

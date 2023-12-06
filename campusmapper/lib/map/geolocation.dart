4197b9931c6a63f0ae4dbbcf8af03c21c7e2e9db
LucaLot <luca.lotito@ontariotechu.net>
2023-12-05 23:04:46 -0500

    Added Map functionality and buttons

--- campusmapper/lib/map/geolocation.dart
+++ campusmapper/lib/map/geolocation.dart
@@ -50,7 +50,6 @@ class GeoLocation {
   Future<LatLng> getPosition() async {
     Position pos = await Geolocator.getCurrentPosition();
     LatLng curLoc = LatLng(pos.latitude, pos.longitude);
-    print(curLoc);
     return curLoc;
   }
 }

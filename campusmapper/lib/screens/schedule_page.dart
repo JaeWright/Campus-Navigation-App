6ef081d5e10d633a2c76022bd71cd1acce35a4ea
BrockD101 <78865978+BrockD101@users.noreply.github.com>
2023-12-08 00:00:44 -0500

    Updated UI and logic

--- campusmapper/lib/screens/schedule_page.dart
+++ campusmapper/lib/screens/schedule_page.dart
@@ -1,3 +1,12 @@
+/*
+Author: Brock Davidge - 100787894
+This page displays the user's weekly course schedule. It listens for changes
+in the ScheduleProvider using the Provider package and organizes courses by day
+of the week. The app bar is styled with a lilac-colored background and white
+bold text. Each day's schedule is presented in a card format, showing course
+names and times. Tapping on a course navigates to the CourseDetailsPage.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:campusmapper/models/sqflite/courses.dart';
@@ -25,15 +34,17 @@ class SchedulePage extends StatelessWidget {
     List<String> weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
 
     return Scaffold(
+      // Inside the SchedulePage widget, update the appBar property
       appBar: AppBar(
-        title: Text('Weekly Schedule'),
-        leading: IconButton(
-          icon: const Icon(Icons.calendar_today),
-          onPressed: () {
-            // Add any functionality you want when the icon is pressed
-            // For example, you can navigate to a specific page or show a dialog
-          },
+        title: Text(
+          'Weekly Schedule',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        backgroundColor: Colors.purple[700], // Lilac-colored background
+        leading: null,
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -48,24 +59,22 @@ class SchedulePage extends StatelessWidget {
                   children: [
                     Text(
                       '$day:',
-                      style: const TextStyle(
+                      style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     if (coursesForDay != null && coursesForDay.isNotEmpty)
                       ...coursesForDay.map(
-                        (course) => ListTile(
+                            (course) => ListTile(
                           title: Text(course.courseName!),
-                          subtitle:
-                              Text('${course.startTime!} - ${course.endTime!}'),
+                          subtitle: Text('${course.startTime!} - ${course.endTime!}'),
                           onTap: () {
                             // Handle tapping on a course to show details
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) =>
-                                    CourseDetailsPage(course: course),
+                                builder: (context) => CourseDetailsPage(course: course),
                               ),
                             );
                           },
@@ -83,3 +92,5 @@ class SchedulePage extends StatelessWidget {
     );
   }
 }
+
+

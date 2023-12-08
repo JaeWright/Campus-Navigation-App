b78775e508f9be0e01fffa300f8174cafea45281
BrockD101 <78865978+BrockD101@users.noreply.github.com>
2023-12-08 11:58:39 -0500

    Update schedule_provider.dart

--- campusmapper/lib/utilities/schedule_provider.dart
+++ campusmapper/lib/utilities/schedule_provider.dart
@@ -1,3 +1,10 @@
+/*
+Author: Brock Davidge - 100787894
+This class manages the user's course schedule. It provides methods to add,
+remove, and get courses from the schedule. It uses ChangeNotifier to notify
+listeners about any changes in the schedule.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:campusmapper/models/sqflite/courses.dart'; // Import your Course model
 
@@ -14,5 +21,10 @@ class ScheduleProvider with ChangeNotifier {
   void removeFromSchedule(Course course) {
     _courses.remove(course);
     notifyListeners();
+  }
+
+  bool isCourseInSchedule(Course course) {
+    // Check if the course is in the schedule list
+    return _courses.contains(course);
   }
 }

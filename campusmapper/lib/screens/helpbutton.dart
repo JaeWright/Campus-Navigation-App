779a89a2bf0f818435f6877667559ae127d8ffeb
SAMUR274 <90654435+SAMUR274@users.noreply.github.com>
2023-12-11 18:36:02 -0500

    ALl Help Butto updated with ICONS

--- campusmapper/lib/screens/helpbutton.dart
+++ campusmapper/lib/screens/helpbutton.dart
@@ -21,20 +21,6 @@ class TutorialPage1 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.info,
-              color: Colors.black,
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => InformationCenterPage()),
-              );
-            },
-          ),
-        ],
         title: Text('Information Center'),
         backgroundColor: Color(0xFF3498DB), // Customize header color
       ),
@@ -48,37 +34,30 @@ class TutorialPage1 extends StatelessWidget {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.newspaper),
             title: Text(
-              'Browse the list of restaurants on Campus.',
+              'Browse the latest campus news and updates.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.library_books),
             title: Text(
-              'Tap on a restaurant for which you want directions or menu.',
+              'Access a wide range of resources available in the campus library.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.contact_phone),
             title: Text(
-              'A pop-up window will appear at the bottom showing Directions and Menu options.',
+              'Find contact information for emergency services and make direct phone calls.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.message),
             title: Text(
-              'Click on Directions to get directions to that restaurant from your current location.',
-              style: TextStyle(fontSize: 18.0),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.check),
-            title: Text(
-              'Click on Menu to view the menu of that restaurant on your screen.',
+              'To get more info on the campus news (perhaps messages or notifications not rendering), assistance is provided on the campus library 24/7.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
@@ -86,9 +65,12 @@ class TutorialPage1 extends StatelessWidget {
       ),
     );
   }
+
 }
 
+
 class TutorialPage2 extends StatelessWidget {
+  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -121,35 +103,35 @@ class TutorialPage2 extends StatelessWidget {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.restaurant_menu),
             title: Text(
               'Browse the list of restaurants on Campus.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.touch_app),
             title: Text(
               'Tap on a restaurant for which you want directions or menu.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.message_outlined),
             title: Text(
               'A pop-up window will appear at the bottom showing Directions and Menu options.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.directions),
             title: Text(
               'Click on Directions to get directions to that restaurant from your current location.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.menu_book),
             title: Text(
               'Click on Menu to view the menu of that restaurant on your screen.',
               style: TextStyle(fontSize: 18.0),
@@ -159,6 +141,7 @@ class TutorialPage2 extends StatelessWidget {
       ),
     );
   }
+
 }
 
 class TutorialPage3 extends StatelessWidget {
@@ -193,38 +176,32 @@ class TutorialPage3 extends StatelessWidget {
               style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
             ),
           ),
+
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.accessible_forward),
             title: Text(
-              'Browse the list of restaurants on Campus.',
+              'Select any location to view detailed accessibility information and find accessible entrances.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.visibility),
             title: Text(
-              'Tap on a restaurant for which you want directions or menu.',
+              'Use the "Find Accessible Entrance" feature to get visual guidance to the entrance.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.info_outline),
             title: Text(
-              'A pop-up window will appear at the bottom showing Directions and Menu options.',
+              'Information on accessible parking spots and restrooms is also available.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.phone_in_talk),
             title: Text(
-              'Click on Directions to get directions to that restaurant from your current location.',
-              style: TextStyle(fontSize: 18.0),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.check),
-            title: Text(
-              'Click on Menu to view the menu of that restaurant on your screen.',
+              'For assistance or emergencies, use the in-app feature to call campus support services directly.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
@@ -232,10 +209,10 @@ class TutorialPage3 extends StatelessWidget {
       ),
     );
   }
+
 }
 
 class TutorialPage4 extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -267,38 +244,37 @@ class TutorialPage4 extends StatelessWidget {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.login),
             title: Text(
               'Make sure to be logged in to access your course and event data!',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.swap_horiz),
             title: Text(
               'Use the toggle to switch between showing your courses and events',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.edit),
             title: Text(
               'When looking at events, tap to edit and hold to delete',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.add),
             title: Text(
               'When looking at events, press the + icon in the appbar to add a new event',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.calendar_today),
             title: Text(
-              'When looking at events, click the calendar icon in the appbar to view your events in '
-              'a calendar view',
+              'When looking at events, click the calendar icon in the appbar to view your events in a calendar view',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
@@ -306,6 +282,7 @@ class TutorialPage4 extends StatelessWidget {
       ),
     );
   }
+
 }
 
 class TutorialPage5 extends StatelessWidget {
@@ -314,24 +291,6 @@ class TutorialPage5 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.map,
-              color: Colors.black,
-            ),
-            onPressed: () {
-              final locationService =
-                  Provider.of<LocationService>(context, listen: false);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (_) => ListMapScreen(
-                        findLocation: const LatLng(0.0, 0.0), type: "None")),
-              );
-            },
-          ),
-        ],
         title: Text('Campus Map'),
         backgroundColor: Color(0xFF008080), // Customize header color
       ),
@@ -345,37 +304,37 @@ class TutorialPage5 extends StatelessWidget {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.my_location),
             title: Text(
-              'Browse the list of restaurants on Campus.',
+              'Allow the app to access your location to show your position on the campus map.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.map),
             title: Text(
-              'Tap on a restaurant for which you want directions or menu.',
+              'Explore the map to view different campus locations. Zoom in and out for a detailed or broad view.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.layers_clear),
             title: Text(
-              'A pop-up window will appear at the bottom showing Directions and Menu options.',
+              'Tap on "Add Icons to Map" to display or hide facilities like washrooms, elevators, and food spots.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.filter_alt),
             title: Text(
-              'Click on Directions to get directions to that restaurant from your current location.',
+              'Use the filters to select which facilities you want to see on the map. Tap "Apply Changes" to update.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.directions_walk),
             title: Text(
-              'Click on Menu to view the menu of that restaurant on your screen.',
+              'Select any location to get walking directions from your current position.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
@@ -383,10 +342,10 @@ class TutorialPage5 extends StatelessWidget {
       ),
     );
   }
+
 }
 
 class TutorialPage6 extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -418,37 +377,37 @@ class TutorialPage6 extends StatelessWidget {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.book),
             title: Text(
-              'Browse the list of restaurants on Campus.',
+              'Use the search bar to find courses by name, number, or instructor.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.add_circle_outline),
             title: Text(
-              'Tap on a restaurant for which you want directions or menu.',
+              'To add a course to your schedule, tap the plus icon next to the course name.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.info_outline),
             title: Text(
-              'A pop-up window will appear at the bottom showing Directions and Menu options.',
+              'For more details about a course, select the course from the list.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.filter_list),
             title: Text(
-              'Click on Directions to get directions to that restaurant from your current location.',
+              'Apply filters to narrow down the search results according to your preferences.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.check),
+            leading: Icon(Icons.star_border),
             title: Text(
-              'Click on Menu to view the menu of that restaurant on your screen.',
+              'Favorite courses for quick access later by tapping the star icon.',
               style: TextStyle(fontSize: 18.0),
             ),
           ),
@@ -456,6 +415,7 @@ class TutorialPage6 extends StatelessWidget {
       ),
     );
   }
+
 }
 
 // Implement TutorialPage2 and TutorialPage3 similarly

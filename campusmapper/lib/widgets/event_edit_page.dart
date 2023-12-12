7780c00f12f878fac7ab38d367437792374ca003
Jaelen Wright <jaelen.wright@ontariotechu.net>
2023-12-11 19:30:47 -0500

    add validation checks for event add/edit

--- campusmapper/lib/widgets/event_edit_page.dart
+++ campusmapper/lib/widgets/event_edit_page.dart
@@ -34,6 +34,8 @@ class EventEditPageState extends State<EventEditPage> {
   late TextEditingController timeController;
   late String weekday;
   late DateTime date;
+  //form key
+  final _eventEditKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -77,96 +79,125 @@ class EventEditPageState extends State<EventEditPage> {
           ),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                //edit event name
-                TextFormField(
-                  controller: eventNameController,
-                  decoration: const InputDecoration(labelText: 'Event Name'),
-                ),
-                //edit location
-                TextFormField(
-                  controller: locationController,
-                  decoration: const InputDecoration(labelText: 'Location'),
-                ),
-                //edit date
-                TextFormField(
-                  readOnly: true,
-                  controller: dateController,
-                  decoration: InputDecoration(
-                    labelText: 'Date',
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.calendar_today),
-                      onPressed: () async {
-                        final DateTime? picked = await showDatePicker(
-                          context: context,
-                          initialDate: widget.date!,
-                          firstDate: DateTime(2023),
-                          lastDate: DateTime(2025),
-                          selectableDayPredicate: (DateTime date) {
-                            // Disable Saturday (day 6) and Sunday (day 7)
-                            return date.weekday != DateTime.saturday &&
-                                date.weekday != DateTime.sunday;
-                          },
-                        );
-                        if (picked != null) {
-                          setState(() {
-                            date = picked;
-                            weekday = convertWeekday(picked);
-                            dateController.text =
-                                "${convertWeekday(picked)} on ${picked.year}-${picked.month}-${picked.day}"; // format in weekday - date
-                          });
-                        }
-                      },
-                    ),
+            child: Form(
+              key: _eventEditKey,
+              child:Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  //edit event name
+                  TextFormField(
+                    controller: eventNameController,
+                    decoration: const InputDecoration(labelText: 'Event Name'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the event name';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-                //edit time
-                TextFormField(
-                  readOnly: true,
-                  controller: timeController,
-                  decoration: InputDecoration(
-                    labelText: 'Time',
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.access_time),
-                      onPressed: () async {
-                        final TimeOfDay? picked = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        );
-                        if (picked != null) {
-                          setState(() {
-                            timeController.text = picked
-                                .format(context); // Format as per requirement
-                          });
-                        }
-                      },
-                    ),
+                  //edit location
+                  TextFormField(
+                    controller: locationController,
+                    decoration: const InputDecoration(labelText: 'Location'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the location';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    //hide keyboard before navigation (causes a renderflex error if not)
-                    FocusScope.of(context).unfocus();
-                    //delay for keyboard to hide before navigation
-                    Future.delayed(const Duration(milliseconds: 750), () {
-                      //pass updated data back to scheduler
-                      var updatedData = {
-                        'eventName': eventNameController.text,
-                        'location': locationController.text,
-                        'weekday': weekday,
-                        'date': date,
-                        'time': timeController.text,
-                      };
-                      Navigator.pop(context, updatedData);
-                    });
-                  },
-                  child: const Text('Save Changes'),
-                ),
-              ],
-            ),
+                  //edit date
+                  TextFormField(
+                    readOnly: true,
+                    controller: dateController,
+                    decoration: InputDecoration(
+                      labelText: 'Date',
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.calendar_today),
+                        onPressed: () async {
+                          final DateTime? picked = await showDatePicker(
+                            context: context,
+                            initialDate: widget.date!,
+                            firstDate: DateTime(2023),
+                            lastDate: DateTime(2025),
+                            selectableDayPredicate: (DateTime date) {
+                              // Disable Saturday (day 6) and Sunday (day 7)
+                              return date.weekday != DateTime.saturday &&
+                                  date.weekday != DateTime.sunday;
+                            },
+                          );
+                          if (picked != null) {
+                            setState(() {
+                              date = picked;
+                              weekday = convertWeekday(picked);
+                              dateController.text =
+                              "${convertWeekday(picked)} on ${picked.year}-${picked.month}-${picked.day}"; // format in weekday - date
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a date (press the calendar icon)';
+                      }
+                      return null;
+                    },
+                  ),
+                  //edit time
+                  TextFormField(
+                    readOnly: true,
+                    controller: timeController,
+                    decoration: InputDecoration(
+                      labelText: 'Time',
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.access_time),
+                        onPressed: () async {
+                          final TimeOfDay? picked = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.now(),
+                          );
+                          if (picked != null) {
+                            setState(() {
+                              timeController.text = picked
+                                  .format(context); // Format as per requirement
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a time (press the clock icon)';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      //hide keyboard before navigation (causes a renderflex error if not)
+                      FocusScope.of(context).unfocus();
+                      //delay for keyboard to hide before navigation
+                      if (_eventEditKey.currentState!.validate()) {
+                      Future.delayed(const Duration(milliseconds: 750), () {
+                        //pass updated data back to scheduler
+                        var updatedData = {
+                          'eventName': eventNameController.text,
+                          'location': locationController.text,
+                          'weekday': weekday,
+                          'date': date,
+                          'time': timeController.text,
+                        };
+                        Navigator.pop(context, updatedData);
+                        });
+                      }
+                    },
+                    child: const Text('Save Changes'),
+                  ),
+                ],
+              ),
+            )
           ),
         ));
   }
